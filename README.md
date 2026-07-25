99e83d6c2939d3cdf933b8dcd15727cfaaf6f4a3
Gaurav Raj <gaurav@thehackersbrain.dev>
2026-07-25 08:17:58 +0530

    added new files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Malware Dev Code
+
 > Code Samples in C/C++/Rust
+
+---
+
+```bash
+g++ dll-gen.cpp -o dll-gen.dll -shared
+```
---
+++ dll-gen.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+
+#pragma comment(lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_PROCESS_DETACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
+
+extern "C"
+{
+    __declspec(dllexport) BOOL WINAPI RunME(void)
+    {
+        MessageBox(
+            NULL,
+            "Hello There <Description>",
+            "Title",
+            MB_OK);
+        return TRUE;
+    }
+}
